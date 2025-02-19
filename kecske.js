2fd78d9a3ed0ab23e2f9d3df60dbad5af17d2bde
Kisváradi Márton (Szf_N_2024_2026) <kisvaradi.marci@gmail.com>
2025-02-19 12:39:06 +0100

    kecske

---
+++ kecske.js
@@ -0,0 +1,19 @@
+console.log("asd")
+const asideElem = document.getElementById("bal");
+
+asideElem.innerHTML += `
+    <img src="kepek/kecske2.png" alt="kecske" onclick="kecskeMozgatas()">
+    <img src="kepek/kaposzta.png" alt="kaposzta" onclick="kaposztaMozgatas()">
+    <img src="kepek/farkas.png" alt="farkas" onclick="farkasMozgatas()">
+    `
+
+const kecskeMozgatas = () => {
+    let articleElem = document.getElementById("csonak")
+    let balKep = document.querySelector('img[alt="kecske"]');
+        if (balKep) {
+            balKep.style.display = 'none';
+            articleElem.innerHTML+= ``
+
+        }
+        console.log(articleElem) 
+}
